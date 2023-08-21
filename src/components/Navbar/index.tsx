d2bcae205ae08dac5137b023305edb49a90206bd
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-20 23:59:32 -0300

    feat: add key down handler

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,27 +1,52 @@
+"use client";
 import { AiOutlineGithub } from "react-icons/ai";
-import { useState } from "react";
-import {
-  Button,
-  Card,
-  Dialog,
-  IconButton,
-  Input,
-  Typography,
-} from "@imagine-ui/react";
+import { useCallback, useEffect, useState } from "react";
+import { Button, Card, IconButton, Typography } from "@imagine-ui/react";
 
 import { MagnifyingGlassIcon, SunIcon } from "@heroicons/react/20/solid";
+import { useDispatch } from "react-redux";
+import { AppDispath, useAppSelector } from "@/redux/store";
+import { changeTheme } from "@/redux/features/theme-slice";
+import { SearchDialog } from "./components/SearchDialog";
 
 function Navbar() {
   const [isOpenSearch, setIsOpenSearch] = useState<boolean>(false);
+
+  const keyDownHandler = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === "k") {
+        setIsOpenSearch(!isOpenSearch);
+      }
+    },
+    [isOpenSearch],
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyDownHandler);
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, [keyDownHandler]);
+
+  const dispatch = useDispatch<AppDispath>();
+  const currentTheme = useAppSelector(
+    (state) => state.themeReducer.value.theme,
+  );
+
+  const handleTheme = useCallback(() => {
+    dispatch(changeTheme(currentTheme === "light" ? "dark" : "light"));
+  }, [currentTheme, dispatch]);
+
   return (
     <>
-      <Dialog isOpen={isOpenSearch} onOpenChange={(e) => setIsOpenSearch(e)}>
-        <Card className="p-4 h-full w-full rounded-md">
-          <Input placeholder="Search components..." size="lg" />
-        </Card>
-      </Dialog>
+      <SearchDialog
+        isOpen={isOpenSearch}
+        onOpenChange={(e) => setIsOpenSearch(e)}
+      />
       <Card
-        className="sticky flex-row z-10 top-0 h-14 w-full justify-between backdrop-blur-2xl bg-white/70 border-b"
+        className={`sticky flex-row z-10 top-0 h-14 w-full rounded-none justify-between backdrop-blur-2xl   ${
+          currentTheme === "dark" ? "bg-zinc-700/50" : "bg-zinc-300/50"
+        }`}
         shadow={false}
       >
         <div className="w-full p-4">
@@ -32,9 +57,10 @@ function Navbar() {
 
         <ul className="flex items-center gap-4 w-[400px]">
           <Button
-            variant="outlined"
+            variant="transparent"
+            color="black"
             size="sm"
-            className="w-[200px] justify-start px-4 hover:bg-gray-200 border-gray-300 hover:border-gray-300"
+            className="w-[200px] justify-start px-4  border border-gray-300"
             onClick={() => setIsOpenSearch(true)}
           >
             <MagnifyingGlassIcon className="w-4 h-4 text-green-500" />
@@ -43,12 +69,13 @@ function Navbar() {
           </Button>
           <li>
             <IconButton
-              color="green"
+              color={currentTheme === "light" ? "green" : "blue"}
               variant="text"
               shadow={false}
               ripple={false}
+              onClick={handleTheme}
             >
-              <SunIcon className="w-5 h-5 00" />
+              <SunIcon className="w-5 h-5" />
             </IconButton>
           </li>
           <li>
