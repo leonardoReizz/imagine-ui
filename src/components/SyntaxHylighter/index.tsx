606394285181a9444a97c9491082601a4f3c3833
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-22 22:55:33 -0300

    fix: define  syntax highlighter default theme

--- src/components/SyntaxHylighter/index.tsx
+++ src/components/SyntaxHylighter/index.tsx
@@ -1,6 +1,5 @@
-import { useAppSelector } from "@/redux/store";
 import { PrismLight } from "react-syntax-highlighter";
-import prism from "react-syntax-highlighter/dist/esm/styles/prism/prism";
+import prism from "react-syntax-highlighter/dist/esm/styles/prism/okaidia";
 import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
 
 PrismLight.registerLanguage("jsx", jsx);
@@ -10,9 +9,6 @@ interface SyntaxHighlighterProps {
 }
 
 export function SyntaxHighlighter({ codeString }: SyntaxHighlighterProps) {
-  const currentTheme = useAppSelector(
-    (props) => props.themeReducer.value.theme,
-  );
   return (
     <PrismLight
       language="jsx"
@@ -21,9 +17,9 @@ export function SyntaxHighlighter({ codeString }: SyntaxHighlighterProps) {
       wrapLongLines={true}
       showLineNumbers={true}
       customStyle={{
-        background: currentTheme === "light" ? "#fff" : "#0010",
+        background: "#27272a",
         border: "none",
-        borderRadius: 0,
+        borderRadius: 8,
         borderBottomRightRadius: "0.375rem",
         borderBottomLeftRadius: "0.375rem",
       }}
