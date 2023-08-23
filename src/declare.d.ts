3e791211b003e5162d6408be8032c9e21367b9f4
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-22 22:53:50 -0300

    declare syntax highlighter

--- src/declare.d.ts
+++ src/declare.d.ts
@@ -1,2 +1,7 @@
 declare module "react-syntax-highlighter";
 declare module "react-syntax-highlighter/dist/esm/languages/prism/jsx";
+declare module "react-syntax-highlighter/dist/esm/styles/prism/prism";
+
+type RefsType = {
+  [key: string]: React.RefObject<HTMLDivElement>;
+};
