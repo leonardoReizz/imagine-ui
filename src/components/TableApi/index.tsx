c537b278e7a040253462bed417920b919c058315
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-22 22:53:12 -0300

    feat: add table api

--- src/components/TableApi/index.tsx
+++ src/components/TableApi/index.tsx
@@ -1,3 +1,5 @@
+import { Typography } from "@imagine-ui/react";
+
 interface TableAPIProps {
   body: {
     id: string;
@@ -5,6 +7,7 @@ interface TableAPIProps {
     type: string;
     default: string;
     description: string;
+    onClickType?: () => void;
   }[];
 }
 
@@ -12,21 +15,46 @@ export function TableAPI({ body }: TableAPIProps) {
   return (
     <table className="w-full text-left">
       <thead>
-        <tr className="border-b">
-          <th>Name</th>
-          <th>Type</th>
-          <th>Default</th>
-          <th>Description</th>
+        <tr className="border-b h-12 dark:border-zinc-700">
+          <th>
+            <Typography variant="small">Name</Typography>
+          </th>
+          <th>
+            <Typography variant="small">Type</Typography>
+          </th>
+          <th>
+            <Typography variant="small">Default</Typography>
+          </th>
+          <th>
+            <Typography variant="small">Description</Typography>
+          </th>
         </tr>
       </thead>
       <tbody>
         {body.map((t) => {
           return (
-            <tr className="border-b h-12" key={t.id}>
-              <td>{t.name}</td>
-              <td>{t.type}</td>
-              <td>{t.default}</td>
-              <td>{t.description}</td>
+            <tr className="border-b h-12  dark:border-zinc-700" key={t.id}>
+              <td>
+                <Typography variant="small">{t.name}</Typography>
+              </td>
+              <td onClick={t.onClickType}>
+                <Typography
+                  variant="small"
+                  className={
+                    t.onClickType
+                      ? "hover:text-green-500 cursor-pointer font-bold"
+                      : ""
+                  }
+                >
+                  {t.type}
+                </Typography>
+              </td>
+              <td>
+                <Typography variant="small">{t.default}</Typography>
+              </td>
+              <td>
+                <Typography variant="small">{t.description}</Typography>
+              </td>
             </tr>
           );
         })}
