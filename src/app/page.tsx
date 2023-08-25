17a8588ca90c4cd7a6af408e435aabb828de7eac
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-25 13:21:15 -0300

    feat: initial landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,253 @@
-import Image from 'next/image'
+"use client";
+import {
+  Button,
+  Card,
+  IconButton,
+  List,
+  ListItem as ImagineListItem,
+  Typography,
+} from "@imagine-ui/react";
+import { useRouter } from "next/navigation";
+import {
+  BellAlertIcon,
+  Cog6ToothIcon,
+  ArrowRightIcon,
+} from "@heroicons/react/20/solid";
+import { ReactNode } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+function ListItem({ children }: { children: ReactNode }) {
+  return (
+    <ImagineListItem
+      ripple={false}
+      className="hover:bg-transparent hover:text-green-500 text-sm h-6"
+    >
+      {children}
+    </ImagineListItem>
+  );
+}
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <main className="flex min-h-screen w-full flex-col  items-center justify-start p-4 gap-32">
+        <Card
+          className="w-full h-full min-h-[600px] max-h-[1000px] flex-col items-center  justify-center gap-4 p-4 lg:flex-row"
+          color="transparent"
+          shadow={false}
+        >
+          <div className="flex flex-col gap-6 flex-1">
+            <Typography variant="h3" className="max-md:text-4xl">
+              Build modern web applications with Flowbite React
+            </Typography>
+            <Typography>
+              Flowbite React is an open-source UI component library built on top
+              of Tailwind CSS with React components and based on the Flowbite
+              Design System
+            </Typography>
+            <div className="max-md:w-full max-md:mb-10">
+              <Button
+                color="green"
+                onClick={() => router.push("/docs/button")}
+                className="max-md:ml-auto max-md:mr-auto"
+              >
+                Get Started <ArrowRightIcon className="h-5 w-5" />
+              </Button>
+            </div>
+          </div>
+          <div className="flex gap-4 h-full w-full p-4 flex-1 justify-start">
+            <div className="flex flex-col gap-4 w-full">
+              <div className="flex gap-4">
+                <Button color="black">SignIn</Button>
+                <IconButton>
+                  <Cog6ToothIcon className="h-4 w-4" />
+                </IconButton>
+              </div>
+              <div>
+                <Button color="red">
+                  Notification <BellAlertIcon className="h-4 w-4" />
+                </Button>
+              </div>
+              <List className="shadow  rounded-md dark:shadow-zinc-600 ">
+                <ImagineListItem>Item 1</ImagineListItem>
+                <ImagineListItem>Item 2</ImagineListItem>
+                <ImagineListItem>Item 3</ImagineListItem>
+                <ImagineListItem>Item 4</ImagineListItem>
+                <ImagineListItem>Item 5</ImagineListItem>
+              </List>
+            </div>
+            {/* <div className="flex flex-col gap-4 w-full min-h-[400px] h-full">
+              <Input label="Email" />
+              <Input placeholder="Password" type="password" />
+            </div> */}
+          </div>
+        </Card>
+        <Card
+          className="flex flex-col gap-20 w-full h-full"
+          color="transparent"
+          shadow={false}
+        >
+          <div className="flex flex-col items-center gap-2">
+            <Typography variant="h6">
+              Integrations with the Tailwind CSS Framework
+            </Typography>
+            <Typography>
+              Boost your workflow by integrating your preferred tools
+              effortlessly using Material Tailwind.
+            </Typography>
+          </div>
+          <div className="flex w-full gap-4 justify-center items-center flex-wrap ">
+            <Card
+              className="border dark:border-zinc-700 hover:bg-zinc-100 hover:dark:bg-zinc-800 transition-all items-center justify-center gap-4 w-full h-[175px] hover:shadow-lg peer p-4 flex-[1_1_250px]"
+              color="transparent"
+            >
+              <Typography variant="h6" bold>
+                React
+              </Typography>
+              <Image alt="" src="react-icon.svg" width={60} height={60} />
+            </Card>
+
+            <Card
+              className="border  dark:border-zinc-700 hover:bg-zinc-100 hover:dark:bg-zinc-800 transition-all items-center justify-center gap-4 w-full h-[175px] hover:shadow-lg peer p-4 flex-[1_1_250px]"
+              color="transparent"
+            >
+              <Typography variant="h6" bold>
+                Create React App
+              </Typography>
+              <Image
+                alt=""
+                src="create-react-app-icon.svg"
+                width={60}
+                height={60}
+              />
+            </Card>
+
+            <Card
+              className="border dark:border-zinc-700 hover:bg-zinc-100 hover:dark:bg-zinc-800 transition-all items-center justify-center gap-4 w-full h-[175px] hover:shadow-lg peer p-4 flex-[1_1_250px]"
+              color="transparent"
+            >
+              <Typography variant="h6" bold>
+                Vite
+              </Typography>
+              <Image alt="" src="vite-icon.svg" width={60} height={60} />
+            </Card>
+
+            <Card
+              className="border dark:border-zinc-700 hover:bg-zinc-100 hover:dark:bg-zinc-800 transition-all items-center justify-center gap-4 w-full h-[175px] hover:shadow-lg peer p-4 flex-[1_1_250px]"
+              color="transparent"
+            >
+              <Typography variant="h6" bold>
+                Next.js
+              </Typography>
+              <Image alt="" src="next-icon.svg" width={60} height={60} />
+            </Card>
+          </div>
+        </Card>
+        <Card
+          className="w-full flex items-center"
+          shadow={false}
+          color="transparent"
+        >
+          <div className="flex flex-col items-center gap-2">
+            <Typography variant="h5">React UI Components</Typography>
+            <Typography className="max-w-[700px]" align="center">
+              Explore the entire collection of over 7 components and interactive
+              elements built with React and Tailwind CSS
+            </Typography>
+          </div>
+          <div
+            className="w-full flex gap-4 items-center justify-center mt-20 flex-wrap"
+            color="transparent"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+            <Card
+              className="w-full max-w-[350px] h-[250px] hover:shadow-lg peer"
+              color="transparent"
+            >
+              <div className="bg-zinc-100  rounded-t-md p-2 ">
+                <Typography>Button</Typography>
+              </div>
+              <div className="w-full h-full peer-hover:bg-zinc-100">a</div>
+            </Card>
+            <Card
+              className="w-full max-w-[350px] h-[250px] hover:shadow-lg peer"
+              color="transparent"
+            >
+              <div className="bg-zinc-100  rounded-t-md p-2 ">
+                <Typography>Button Icon</Typography>
+              </div>
+              <div className="w-full h-full peer-hover:bg-zinc-100">a</div>
+            </Card>
+            <Card
+              className="w-full max-w-[350px] h-[250px] hover:shadow-lg peer"
+              color="transparent"
+            >
+              <div className="bg-zinc-100  rounded-t-md p-2 ">
+                <Typography>List</Typography>
+              </div>
+              <div className="w-full h-full peer-hover:bg-zinc-100">a</div>
+            </Card>
+            <Card
+              className="w-full max-w-[350px] h-[250px] hover:shadow-lg peer"
+              color="transparent"
+            >
+              <div className="bg-zinc-100  rounded-t-md p-2 ">
+                <Typography>Input</Typography>
+              </div>
+              <div className="w-full h-full peer-hover:bg-zinc-100">a</div>
+            </Card>
+          </div>
+        </Card>
+        <Card
+          className="w-full flex flex-col gap-10"
+          shadow={false}
+          color="transparent"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Typography variant="h5">Join our community</Typography>
+            <Typography align="center" className="max-w-[800px]">
+              Visit our communities to see the latest updates and news regarding
+              our product. Whether you have questions, ideas to contribute, or
+              simply want to connect with like-minded individuals, our
+              communities provide a vibrant and supportive environment for your.
+            </Typography>
+          </div>
+          <div className="flex justify-center  w-full gap-4 flex-wrap">
+            <Link
+              href="https://github.com/leonardoReizz/imagine-ui"
+              target="_blank"
+              className="w-full max-w-[250px] h-[175px] flex-[1_1_250px]"
+            >
+              <Card
+                className="border items-center justify-center gap-4 w-full h-full hover:shadow-lg peer p-4"
+                color="transparent"
+              >
+                <Typography variant="h6" bold>
+                  Git Hub
+                </Typography>
+                <Image alt="" src="github-icon.svg" width={60} height={60} />
+              </Card>
+            </Link>
+            <Link
+              href="https://discord.gg/y2UAthfdkb"
+              target="_blank"
+              className="w-full max-w-[250px] h-[175px] flex-[1_1_250px]"
+            >
+              <Card
+                className="border items-center justify-center gap-4 w-full h-full hover:shadow-lg peer p-4"
+                color="transparent"
+              >
+                <Typography variant="h6" bold>
+                  Discord
+                </Typography>
+                <Image alt="" src="discord-icon.svg" width={60} height={60} />
+              </Card>
+            </Link>
+          </div>
+        </Card>
+      </main>
+    </>
+  );
 }
