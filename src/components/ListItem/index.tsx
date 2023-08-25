8552960683ab208af720eda03e3fbcdb47d0b743
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-25 13:22:18 -0300

    feat: add imagine list item

---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,23 @@
+import { CSSProperties, MouseEventHandler, ReactNode } from "react";
+import { ListItem as ImagineListItem } from "@imagine-ui/react";
+
+export function ListItem({
+  children,
+  onClick,
+  style,
+}: {
+  children: ReactNode;
+  onClick?: MouseEventHandler<HTMLDivElement>;
+  style?: CSSProperties | undefined;
+}) {
+  return (
+    <ImagineListItem
+      ripple={false}
+      onClick={onClick}
+      className="hover:bg-transparent hover:text-green-500 text-sm h-6"
+      style={style}
+    >
+      {children}
+    </ImagineListItem>
+  );
+}
