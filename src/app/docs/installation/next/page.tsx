894c5ba092f0b0e9eb5c2c3a223429b73986963e
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-27 00:48:32 -0300

    feat: initial next installation docs

--- src/app/docs/installation/next/page.tsx
+++ src/app/docs/installation/next/page.tsx
@@ -1,3 +1,90 @@
+"use client";
+import Component from "@/components/ComponentPreview";
+import { ScrollSection } from "@/components/ScroolSection";
+import { SyntaxHighlighter } from "@/components/SyntaxHylighter";
+import {
+  nextImportButtonCodeString,
+  nextInstallNpmCodeString,
+  nextInstallPnpmCodeString,
+  nextTailwindConfigCodeString,
+  nextThemeProvider,
+  nextThemeProviderCodeString,
+} from "@/utils/codeString/nextInstallation";
+import { Typography } from "@imagine-ui/react";
+
 export default function InstallationNext() {
-  return <div>Next</div>;
+  return (
+    <>
+      <ScrollSection className="mt-10">
+        <Typography variant="h6">Material Tailwind with Next.js</Typography>
+        <Typography variant="sub-paragraph" className="mt-4">
+          Learn how to setup and install @imagine-ui/react with Next.js. First
+          you need to create a new project using next.js, for more details check
+          the Next.JS Official Documentation npx create-next-app@latest Then you
+          need to install Tailwind CSS since @imagine-ui/react is working with
+          Tailwind CSS classes and you need to have Tailwind CSS installed on
+          your project. Check Tailwind CSS Installation for next.js on Tailwind
+          CSS Documentation.
+        </Typography>
+      </ScrollSection>
+      <ScrollSection className="mt-10">
+        <Component.Container
+          className="h-fit"
+          title="Using NPM"
+          description="Install @imagine-ui/react as a dependency using NPM by running the
+          following command: npm i @imagine-ui/react"
+        >
+          <SyntaxHighlighter codeString={nextInstallNpmCodeString} />
+        </Component.Container>
+      </ScrollSection>
+      <ScrollSection>
+        <Component.Container
+          className="h-fit"
+          title="Using Yarn"
+          description="Install @imagine-ui/react as a dependency using Yarn by running
+        the following command:"
+        >
+          <SyntaxHighlighter codeString={nextInstallPnpmCodeString} />
+        </Component.Container>
+      </ScrollSection>
+      <ScrollSection>
+        <Component.Container
+          className="h-fit"
+          title="Using PNPM"
+          description=" Install @imagine-ui/react as a dependency using PNPM by running
+          the following command:"
+        >
+          <SyntaxHighlighter codeString={nextInstallPnpmCodeString} />
+        </Component.Container>
+      </ScrollSection>
+      <ScrollSection>
+        <Component.Container
+          className="h-full"
+          title="TailwindCSS Configurations"
+          description="Multiple button designs available to suit different interface needs."
+        >
+          <SyntaxHighlighter codeString={nextTailwindConfigCodeString} />
+        </Component.Container>
+      </ScrollSection>
+
+      <ScrollSection className="mt-10">
+        <Component.Container
+          className="h-fit"
+          title="With Next.js 13 appDir and Server Components"
+          description="Multiple button designs available to suit different interface needs."
+        >
+          <SyntaxHighlighter codeString={nextImportButtonCodeString} />
+        </Component.Container>
+      </ScrollSection>
+
+      <ScrollSection className="mt-14">
+        <Component.Container
+          title="Theme Provider"
+          description="@imagine-ui/react comes with a theme provider that set's the default theme/styles for components or to provide your own theme/styles to your components. You need to wrap your entire application with the ThemeProvider coming from @imagine-ui/react. On the pages/_app put the code bellow."
+        >
+          <SyntaxHighlighter codeString={nextThemeProviderCodeString} />
+        </Component.Container>
+      </ScrollSection>
+    </>
+  );
 }
--- src/app/docs/installation/page.tsx
+++ src/app/docs/installation/page.tsx
@@ -8,92 +8,89 @@ export default function Installation() {
   return (
     <div className="flex flex-col gap-16 mt-10">
       <div className="flex flex-col gap-4">
-        <Typography variant="h6">Get Started with Material Tailwind</Typography>
+        <Typography variant="h6">
+          Commencing with Imagine UI and Tailwin
+        </Typography>
         <Typography variant="sub-paragraph">
-          Material Tailwind is a free and open-source components library for
-          ReactJS and Tailwind CSS inspired by Material Design.
+          Imagine UI stands as a proprietary compilation of components tailored
+          for ReactJS and Tailwind CSS, meticulously crafted to offer
+          streamlined solutions.
         </Typography>
       </div>
       <div className="flex flex-col gap-4">
-        <Typography variant="h6">Installation</Typography>
+        <Typography variant="h6">Setup</Typography>
         <Typography variant="sub-paragraph">
-          Learn how to use @material-tailwind/react components from this
-          documentation to quickly and easily create elegant and flexible pages
-          using Tailwind CSS.
+          Delve into the know-how of harnessing the potential of
+          @imagine-ui/react components by perusing this comprehensive
+          documentation, facilitating the creation of graceful, adaptable pages
+          seamlessly integrated with Tailwind CSS.
         </Typography>
         <Typography>
-          @material-tailwind/react is working with Tailwind CSS classes and you
-          need to have Tailwind CSS installed on your project - Tailwind CSS
-          Installation Guide.
+          @imagine-ui/react seamlessly interfaces with Tailwind CSS classes;
+          hence, the prior installation of Tailwind CSS within your project
+          infrastructure is a prerequisite. The Tailwind CSS Installation Guide
+          is at your disposal to guide you through this process.
         </Typography>
       </div>
       <div className="flex flex-col gap-4">
-        <Typography variant="h6">Frameworks Integration</Typography>
+        <Typography variant="h6">Harmonizing with Frameworks</Typography>
         <Typography variant="sub-paragraph">
-          Framework-specific guides that cover our recommended approach to
-          installing @material-tailwind/react in a number of popular
-          environments. Select your preferred framework from the list below and
-          follow the instructions.
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <Typography variant="sub-paragraph">
+                Our dedicated guides for diverse frameworks expound on the
+                recommended methodology for incorporating @imagine-ui/react
+                within different prominent ecosystems. Make a selection from the
+                list of favored frameworks below, then proceed to implement the
+                specified instructions.
+              </Typography>
+              <Typography variant="sub-paragraph">
+                Syncing with the Tailwind CSS Framework Elevate your workflow
+                efficacy by harmoniously integrating your preferred tools,
+                empowered by the versatility of Imagine UI.
+              </Typography>
+            </div>
+            <div className="flex w-full gap-4 justify-center items-center flex-wrap ">
+              <Link href="/docs/installation/cra" className="flex-[1_1_150px]">
+                <Card
+                  className="border  dark:border-zinc-700 hover:bg-zinc-100 hover:dark:bg-zinc-800 transition-all items-center justify-center gap-4 w-full h-[175px] hover:shadow-lg peer p-4 "
+                  color="transparent"
+                >
+                  <Image
+                    alt=""
+                    src="/create-react-app-icon.svg"
+                    width={60}
+                    height={60}
+                  />
+                </Card>
+              </Link>
+              <Link href="/docs/installation/vite" className="flex-[1_1_150px]">
+                <Card
+                  className="border dark:border-zinc-700 hover:bg-zinc-100 hover:dark:bg-zinc-800 transition-all items-center justify-center gap-4 w-full h-[175px] hover:shadow-lg peer p-4 "
+                  color="transparent"
+                >
+                  <Image alt="" src="/vite-icon.svg" width={60} height={60} />
+                </Card>
+              </Link>
+              <Link href="/docs/installation/next" className="flex-[1_1_150px]">
+                <Card
+                  className="border dark:border-zinc-700 hover:bg-zinc-100 hover:dark:bg-zinc-800 transition-all items-center justify-center gap-4 w-full h-[175px] hover:shadow-lg peer p-4 "
+                  color="transparent"
+                >
+                  <Image alt="" src="/next-icon.svg" width={60} height={60} />
+                </Card>
+              </Link>
+            </div>
+          </div>
         </Typography>
       </div>
       <div className="flex flex-col gap-4">
         <Typography variant="h6">Typescript</Typography>
         <Typography variant="sub-paragraph">
-          @material-tailwind/react is a TypeScript based components library and
-          you can easily use it in your TypeScript based projects.
+          The essence of @imagine-ui/react is rooted in components founded upon
+          TypeScript, serving as a facilitator for effortless amalgamation into
+          projects centered around the TypeScript framework.
         </Typography>
-      </div>
-      <div className="flex flex-col gap-4">
-        <Typography variant="h6">Frameworks Integration</Typography>
-        <Typography variant="sub-paragraph">
-          Framework-specific guides that cover our recommended approach to
-          installing @material-tailwind/react in a number of popular
-          environments. Select your preferred framework from the list below and
-          follow the instructions.
-          https://tailwindcss.com/docs/installation/framework-guides?ref=material-tailwind
-        </Typography>
-      </div>
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
-          <Typography variant="h6">
-            Integrations with the Tailwind CSS Framework
-          </Typography>
-          <Typography variant="sub-paragraph">
-            Boost your workflow by integrating your preferred tools effortlessly
-            using Material Tailwind.
-          </Typography>
-        </div>
-        <div className="flex w-full gap-4 justify-center items-center flex-wrap ">
-          <Link href="/docs/installation/cra" className="flex-[1_1_150px]">
-            <Card
-              className="border  dark:border-zinc-700 hover:bg-zinc-100 hover:dark:bg-zinc-800 transition-all items-center justify-center gap-4 w-full h-[175px] hover:shadow-lg peer p-4 "
-              color="transparent"
-            >
-              <Image
-                alt=""
-                src="/create-react-app-icon.svg"
-                width={60}
-                height={60}
-              />
-            </Card>
-          </Link>
-          <Link href="/docs/installation/vite" className="flex-[1_1_150px]">
-            <Card
-              className="border dark:border-zinc-700 hover:bg-zinc-100 hover:dark:bg-zinc-800 transition-all items-center justify-center gap-4 w-full h-[175px] hover:shadow-lg peer p-4 "
-              color="transparent"
-            >
-              <Image alt="" src="/vite-icon.svg" width={60} height={60} />
-            </Card>
-          </Link>
-          <Link href="/docs/installation/next" className="flex-[1_1_150px]">
-            <Card
-              className="border dark:border-zinc-700 hover:bg-zinc-100 hover:dark:bg-zinc-800 transition-all items-center justify-center gap-4 w-full h-[175px] hover:shadow-lg peer p-4 "
-              color="transparent"
-            >
-              <Image alt="" src="/next-icon.svg" width={60} height={60} />
-            </Card>
-          </Link>
-        </div>
       </div>
     </div>
   );
