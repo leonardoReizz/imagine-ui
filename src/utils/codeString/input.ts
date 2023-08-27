8b9c71ae4dd973182aa73a41a3dd1e7bcd32bba8
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-27 18:51:25 -0300

    feat: add inpurt code string

--- src/utils/codeString/input.ts
+++ src/utils/codeString/input.ts
@@ -6,66 +6,79 @@ export function InputExample() {
   )
 }`;
 
-export const buttonRippleEffectCodeString = `function Buttons() {
-  <Button color="green" className="px-4 normal-case">
-    Ripple ON
-  </Button>
-  <Button color="green" ripple={false} className="px-4 normal-case">
-    Ripple OFF
-  </Button>
+export const inputVariantCodeString = `function InputVariant() {
+  return (
+    <Input label="Teste" variant="outlined" />
+    <Input label="Teste" variant="standart" />
+    <Input label="Teste" variant="static" />
+  )
 }`;
 
-export const buttonShadowCodeString = `function Buttons() {
-  <Button variant="solid" color="green" className="px-4 normal-case">
-    Shadow ON
-  </Button>
-  <Button color="green" className="px-4 normal-case" shadow={false}>
-    Shadow OFF
-  </Button>
+export const inputWithIconCodeString = `function InputWithIcon() {
+  return (
+    <Input
+      label="With Icon"
+      icon={
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+        </svg>
+      }
+    />
+  )
 }`;
 
-export const buttonColorsCodeString = `function Buttons() {
-  <Button color="blue" className="px-4 normal-case">
-    Blue
-  </Button>
-  <Button color="green" className="px-4 normal-case">
-    Green
-  </Button>
-  <Button color="red" className="px-4 normal-case">
-    Red
-  </Button>
-  <Button color="purple" className="px-4 normal-case">
-    Purple
-  </Button>
+export const inputColorsCodeString = `function InputColor() {
+  return (
+    <Input label="Blue" color="blue"  />
+    <Input label="Green" color="green"  />
+    <Input label="Red" color="red"  />
+    <Input label="Purple" color="purple"  />
+  )
 }`;
 
-export const buttonNotificationCodeString = `function Buttons() {
-  <Button color="blue" className="px-4 normal-case">
-    <UserIcon className="w-4 h-4" /> 
-    User
-  </Button>
-  <Button color="green" className="px-4 normal-case">
-    Settings
-    <Cog6ToothIcon className="w-4 h-4" />
-  </Button>
-  <Button color="red" className="px-4 normal-case">
-    Notification
-    <BellAlertIcon className="w-4 h-4" />
-  </Button>
+export const inputSizesCodeString = `function InputSize() {
+  return (
+    <Input label="SM" size="sm" />
+    <Input label="MD" size="md" />
+    <Input label="LG" size="lg" />
+  )
+}`;
+
+export const inputDisableCodeString = `function InputDisabled() {
+  return(
+    <Input label="Disabled" disabled />
+  )
 }`;
 
 export const inputImportCodeString = `import { Input } from "@imagine-io/react"`;
 
 export const inputColorTypeCodeString = `type InputColor =
-"red"
-| "green"
 | "blue"
-| "amber"
+| "red"
+| "green"
 | "purple"
 | "orange"
+| "amber"
 | "pink";`;
 
-export const inputVariantTypeCodeString = `type InputVariant =
-"outlined"
-| "solid"
-| "static"`;
+export const inputSizeTypeCodeString = `type  InputSize = "sm" | "md" | "lg";`;
+
+export const inputVariantTypeCodeString = `type InputVariant = "outlined" | "static" | "standart"`;
+
+export const inputIconPropsTypeCodeString = `type IconProps = {
+  className?: string;
+};`;
+
+export const inputContainerPropsTypeCodeString = `type ContainerProps = {
+  className?: string;
+};`;
+
+export const inputlabelPropsTypeCodeString = `type LabelProps = {
+  className?: string;
+};`;
