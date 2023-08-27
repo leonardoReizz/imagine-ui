b17827e16197cfb0220d67a0ebb7592b293c1ae7
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-27 18:52:36 -0300

    add syntax highlighter declaration

--- src/declare.d.ts
+++ src/declare.d.ts
@@ -1,6 +1,7 @@
 declare module "react-syntax-highlighter";
 declare module "react-syntax-highlighter/dist/esm/languages/prism/jsx";
 declare module "react-syntax-highlighter/dist/esm/styles/prism/prism";
+declare module "react-syntax-highlighter/dist/esm/styles/prism/okaidia";
 
 type RefsType = {
   [key: string]: React.RefObject<HTMLDivElement>;
