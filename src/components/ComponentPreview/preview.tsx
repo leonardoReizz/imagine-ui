5ad1ed565481048258877d6200ba1c334759223d
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-29 20:00:51 -0300

    change redux to context

--- src/components/ComponentPreview/preview.tsx
+++ src/components/ComponentPreview/preview.tsx
@@ -6,8 +6,8 @@ import {
   CheckBadgeIcon,
 } from "@heroicons/react/20/solid";
 import { ReactNode, useCallback, useState } from "react";
-import { useAppSelector } from "@/redux/store";
 import { SyntaxHighlighter } from "../SyntaxHylighter";
+import { useThemeContext } from "@/hooks/useThemeContext";
 
 interface PreviewProps {
   codeString: string;
@@ -17,9 +17,7 @@ interface PreviewProps {
 export function Preview({ children, codeString }: PreviewProps) {
   const [state, setState] = useState<number>(0);
   const [copiedCode, setCopiedCode] = useState<boolean>(false);
-  const currentTheme = useAppSelector(
-    (state) => state.themeReducer.value.theme,
-  );
+  const { theme } = useThemeContext();
 
   const handleCopyCode = useCallback(() => {
     navigator.clipboard.writeText(codeString);
@@ -34,14 +32,14 @@ export function Preview({ children, codeString }: PreviewProps) {
     <div className="flex gap-4 items-start justify-center w-full h-full ">
       <Card
         className={`h-full rounded-md border p-0 w-full ${
-          state === 1 || currentTheme === "dark"
+          state === 1 || theme === "dark"
             ? "bg-zinc-800 border-zinc-700"
             : "bg-white"
         }`}
       >
         <div
           className={`flex items-center justify-between w-full py-1 border-b ${
-            currentTheme === "dark" || state === 1
+            theme === "dark" || state === 1
               ? "border-zinc-700"
               : "border-zinc-200"
           }`}
