53025b702f235e76c0ce182ef5823c0bf684d1a0
Leonardo Reis <amarantee.reis@gmail.com>
2023-08-29 19:57:46 -0300

    feat: theming search dialog

--- src/components/Navbar/components/SearchDialog.tsx
+++ src/components/Navbar/components/SearchDialog.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Card,
   Dialog,
@@ -6,7 +7,6 @@ import {
   ListItem,
   Typography,
 } from "@imagine-ui/react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -36,28 +36,28 @@ const docs = [
 
 const pages = [
   {
-    to: "/docs/buttons",
+    to: "/docs/button",
     name: "Button",
+  },
+  {
+    to: "/docs/checkbox",
+    name: "Checkbox",
   },
   {
     to: "/docs/iconButton",
     name: "Icon Button",
   },
   {
-    to: "/docs/list",
-    name: "List",
-  },
-  {
-    to: "/docs/buttons",
-    name: "Button",
-  },
-  {
-    to: "/docs/iconButton",
-    name: "Icon Button",
+    to: "/docs/input",
+    name: "Input",
   },
   {
     to: "/docs/list",
     name: "List",
+  },
+  {
+    to: "/docs/typography",
+    name: "Typography",
   },
 ];
 
@@ -79,50 +79,70 @@ export function SearchDialog({ isOpen, onOpenChange }: SearchDialogProps) {
   }
 
   return (
-    <Dialog
-      isOpen={isOpen}
-      onOpenChange={(e) => onOpenChange(e)}
-      height="100%"
-      maxHeight="500px"
-    >
-      <Card className="p-4 h-full max-h-[500px] w-full rounded-md">
-        <Input
-          size="lg"
-          placeholder="Search components..."
-          onChange={(e) => setSearchValue(e.target.value)}
-          value={searchValue}
-        />
-        <div className="mt-4">
-          <Typography>Docs</Typography>
-          <List className="overflow-auto">
-            {filteredDocs.map((page) => {
-              return (
-                <ListItem
-                  key={page.name}
-                  onKeyDown={(e) => e.key === "Enter" && handlePage(page.to)}
-                >
-                  <Typography>{page.name}</Typography>
-                </ListItem>
-              );
-            })}
-          </List>
-        </div>
-        <div>
-          <Typography>Components</Typography>
-          <List className="overflow-auto">
-            {filteredPages.map((page) => {
-              return (
-                <ListItem
-                  key={page.name}
-                  onKeyDown={(e) => e.key === "Enter" && handlePage(page.to)}
-                >
-                  <Typography>{page.name}</Typography>
-                </ListItem>
-              );
-            })}
-          </List>
-        </div>
-      </Card>
-    </Dialog>
+    <>
+      <Dialog
+        isOpen={isOpen}
+        onOpenChange={(e) => onOpenChange(e)}
+        height="100%"
+        maxHeight="500px"
+      >
+        <Card
+          className="flex flex-col p-4 h-full max-h-[500px] w-full rounded-md dark:bg-zinc-900"
+          tabIndex={-1}
+        >
+          <Input
+            size="lg"
+            placeholder="Search components..."
+            onChange={(e) => setSearchValue(e.target.value)}
+            value={searchValue}
+          />
+          <div className="max-h-[400px] overflow-auto">
+            {filteredDocs.length > 0 && (
+              <div className="mt-4">
+                <Typography>Docs</Typography>
+                <List className="overflow-auto" shadow={false}>
+                  {filteredDocs.map((page) => {
+                    return (
+                      <ListItem
+                        key={page.name}
+                        onKeyDown={(e) =>
+                          e.key === "Enter" && handlePage(page.to)
+                        }
+                        onClick={() => handlePage(page.to)}
+                        className="bg-zinc-100 h-14 dark:bg-zinc-800 dark:hover:bg-zinc-700"
+                      >
+                        <Typography>{page.name}</Typography>
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              </div>
+            )}
+            <hr className="mt-4 mb-4 dark:border-zinc-700" />
+            {filteredPages.length > 0 && (
+              <div>
+                <Typography>Components</Typography>
+                <List className="overflow-auto" shadow={false}>
+                  {filteredPages.map((page) => {
+                    return (
+                      <ListItem
+                        key={page.name}
+                        onKeyDown={(e) =>
+                          e.key === "Enter" && handlePage(page.to)
+                        }
+                        onClick={() => handlePage(page.to)}
+                        className="bg-zinc-100 h-14 dark:bg-zinc-800"
+                      >
+                        <Typography>{page.name}</Typography>
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              </div>
+            )}
+          </div>
+        </Card>
+      </Dialog>
+    </>
   );
 }
